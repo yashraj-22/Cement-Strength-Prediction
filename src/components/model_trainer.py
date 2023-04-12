45c9e5b2a62384bc25e6ea98377da579c28d2629
Yashraj <yrs22012@gmail.com>
2023-04-12 17:37:47 +0530

    Hyperparameter Tuning Done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -42,16 +42,55 @@ class ModelTrainer:
                 "Lasso": Lasso(),
                 "Ridge": Ridge(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
-                "Random Forest": RandomForestRegressor(),
+                "Random Forest": RandomForestRegressor(bootstrap=False),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "XGBRegressor": XGBRegressor(),
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            params={
+                "Random Forest":{
+                    "n_estimators": [100, 200, 500, 600, 800, 900],
+                    "criterion": ['mse','mae'],
+                    "max_depth": [None, 2, 3, 5, 7, 8],
+                    "min_samples_split": [2, 5, 7, 9, 10],
+                    "min_samples_leaf": [1, 2, 5, 7, 9, 10],
+                    "max_features": ["auto", "sqrt", "log2"]
+                },
+                "Gradient Boosting":{
+                    "n_estimators": [100, 200, 500, 600, 800, 900],
+                    "criterion": ['friedman_mse', 'mse', 'mae'],
+                    "learning_rate": [0.05,0.10,0.15,0.20,0.25,0.30],
+                    "max_depth": [None, 2, 3, 5, 7, 8],
+                    "min_samples_split": [2, 5, 7, 9, 10],
+                    "min_samples_leaf": [1, 2, 5, 7, 9, 10]
+                },
+                "Linear Regression":{},
+                "Lasso":{},
+                "Ridge":{},
+                "K-Neighbors Regressor":{},
+                "Decision Tree":{},
+                "AdaBoost Regressor":{},
+                "XGBRegressor":{
+                    'tree_method': ['exact','hist','gpu_hist','approx'],
+                    'learning_rate' : [0.05,0.10,0.15,0.20,0.25,0.30],
+                    'max_depth' : [ 3, 4, 5, 6, 8, 10, 12, 15],
+                    'min_child_weight' : [ 1, 3, 5, 7 ],
+                    'gamma': [ 0.0, 0.1, 0.2 , 0.3, 0.4 ],
+                    'colsample_bytree' : [ 0.3, 0.4, 0.5 , 0.7 ],
+                    'n_estimators':[300,400,500,600,700,800,900]
+                },
+                "CatBoosting Regressor":{
+                    'depth'          : [4,5,6,7,8,9, 10],
+                    'learning_rate' : [0.01,0.02,0.03,0.04],
+                    'iterations'    : [300,400,500,600,700,800,900]
+                }
+                
+            }
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=params)
             
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
